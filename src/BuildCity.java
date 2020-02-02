8dc16fc3787e8d36f5b6454dcd0c0ae34d390f06
Steven Harradine <stevenharradine@gmail.com>
2020-02-02 13:54:32 -0500

    Recommend city names from list

--- src/BuildCity.java
+++ src/BuildCity.java
@@ -3,7 +3,12 @@
  */
 package stevenharradine.jciv;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BuildCity extends Action {
 	public BuildCity (Unit newParentUnit) {
@@ -19,7 +24,9 @@ public class BuildCity extends Action {
 
 		do {
 			String title = isCityNameExist ? "Error: City " + cityName + " already exists" : "New city";
-			TextInput cityNameInput = new TextInput (title, "Enter city name" , "kanata", this.getParentUnit()); 
+			String recommendedCityName = getRecommendedCityName();
+			TextInput cityNameInput = new TextInput (title, "Enter city name" , recommendedCityName, this.getParentUnit());
+
 	 		cityName = cityNameInput.getInput();
 
 	 		isCityNameExist = JCiv.map.isCityNameExist(cityName);
@@ -35,6 +42,33 @@ public class BuildCity extends Action {
 		tile.deleteUnit();
 
 		return true;
+	}
+
+	public String getRecommendedCityName () {
+		String str;
+		List<String> list = new ArrayList<String>();
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("data/citylist.txt"));
+			while((str = in.readLine()) != null){
+				list.add(str);
+			}
+		} catch (Exception e) {
+			System.out.println (e);
+		}
+
+		String[] stringArr = list.toArray(new String[0]);
+
+		for (int i = 0; i < stringArr.length; i++) {
+			boolean isCityNameUnique = !JCiv.map.isCityNameExist(stringArr[i]);
+
+			if (isCityNameUnique) {
+				return stringArr[i];
+			}
+
+		}
+
+		return "";
 	}
 
 	public void updateLabels () {
